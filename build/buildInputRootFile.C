edbc9fb8ea51faa2b5fe35de648e56470e7662c2
Adrien Blanchet <adrien.blanchet@live.fr>
2024-04-09 15:51:33 +0200

    adding binnings

--- build/buildInputRootFile.C
+++ build/buildInputRootFile.C
@@ -1,13 +1,72 @@
 
+int nEvents{1000000};
+
+struct Event{
+  int reaction{-1};
+  int topology{-1};
+  double enu{std::nan("unset")};
+  double pmu{std::nan("unset")};
+  double pp{std::nan("unset")};
+  double asimovWeight{0.05};
+};
 
 void buildInputRootFile(){
 
-  auto* file = TFile::Open("./inputs/fitter/likelihood/datasets/input.root", "RECREATE");
-  auto* tree = TTree::TTree("events", "events");
+  gRandom = new TRandom3(202404); // Initialize with a UUID;
+
+  auto* file = TFile::Open("./inputs/fitter/likelihood/datasets/mydataset.root", "RECREATE");
+  auto* tree = new TTree("events", "events");
+
+  Event e{};
+
+  tree->Branch("enu", &e.enu);
+  tree->Branch("pmu", &e.pmu);
+  tree->Branch("pp", &e.pp);
+  tree->Branch("reaction", &e.reaction);
+  tree->Branch("topology", &e.topology);
+  tree->Branch("asimovWeight", &e.asimovWeight);
+
+  double fraction{0.1}; // shape2/shape1
+  auto fShape1 = std::make_unique<TF1>("fShape1", "ROOT::Math::chisquared_pdf(x, 4)", 0, 15);
+  auto fShape2 = std::make_unique<TF1>("fShape2", "ROOT::Math::chisquared_pdf(x, 5)", 0, 15);
+
+  double fluct;
+  for( int iEvent = 0 ; iEvent < nEvents ; iEvent++ ){
+
+    gRandom->Rndm() > fraction ? e.reaction = 1 : e.reaction = 2;
+
+    if(e.reaction == 1){
+      e.enu = fShape1->GetRandom(gRandom);
+      e.pmu = std::max( 0., e.enu*(0.95 + 0.05*gRandom->Gaus(0, 1)) - 0.1 );
+      e.pp = 0.1*fShape1->GetRandom(gRandom);
+
+      fluct = gRandom->Rndm();
+      if     ( fluct < 0.7 ){ e.topology = 1; }
+      else if( fluct < 0.85 ){ e.topology = 2; }
+      else if( fluct < 0.95 ){ e.topology = 3; }
+      else{ e.topology = -1; } // invisible
+    }
+    if(e.reaction == 2){
+      e.enu = fShape2->GetRandom(gRandom);
+      e.pmu = std::max( 0., e.enu*(0.8 + 0.1*gRandom->Gaus(0, 1)) - 0.2 );
+      e.pp = 0.1*fShape2->GetRandom(gRandom);
+
+      fluct = gRandom->Rndm();
+      if     ( fluct < 0.5 ){ e.topology = 2; }
+      else if( fluct < 0.75 ){ e.topology = 3; }
+      else if( fluct < 0.85 ){ e.topology = 4; }
+      else{ e.topology = -1; }
+    }
+
+    tree->Fill();
+  }
 
 
-  generatedInputFile->Close();
+  file->WriteObject(tree, "events");
+  file->Close();
+  std::cout << "File writen in " << file->GetPath() << std::endl;
 
+  exit(0);
 }
 
 
