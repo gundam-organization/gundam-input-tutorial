edbaf3e59ba1a6faf3a5aa5ba0e4edafdb040c20
Adrien Blanchet <adrien.blanchet@live.fr>
2024-04-09 10:40:52 +0200

    adding a few options

--- build/buildInputRootFile.C
+++ build/buildInputRootFile.C
@@ -2,8 +2,8 @@
 
 void buildInputRootFile(){
 
-  auto* generatedInputFile = TFile::Open("./inputs/fitter/likelihood/datasets/input.root", "RECREATE");
-
+  auto* file = TFile::Open("./inputs/fitter/likelihood/datasets/input.root", "RECREATE");
+  auto* tree = TTree::TTree("events", "events");
 
 
   generatedInputFile->Close();
