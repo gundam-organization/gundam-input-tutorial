66d51f8d5e011bc080d75d96a51ef01bc122c810
Adrien Blanchet <adrien.blanchet@live.fr>
2024-08-03 13:36:41 +0200

    adjusting rates

--- build/buildInputRootFile.C
+++ build/buildInputRootFile.C
@@ -33,24 +33,28 @@ void buildInputRootFile(){
   double buffer;
   for( int iEvent = 0 ; iEvent < nEvents ; iEvent++ ){
 
+    // let's have an interaction with a given reaction:
     buffer = gRandom->Rndm();
-    if     ( buffer < 0.7 ){ e.reaction = 1; }
-    else if( buffer < 0.9 ){ e.reaction = 2; }
+    if     ( buffer < 0.6  ){ e.reaction = 1; }
+    else if( buffer < 0.8  ){ e.reaction = 2; }
     else if( buffer < 0.95 ){ e.reaction = 3; }
     else if( buffer < 0.98 ){ e.reaction = 4; }
     else if( buffer < 0.99 ){ e.reaction = 5; }
-    else{ e.reaction = 6; }
+    else                    { e.reaction = 6; }
 
+    // reaction 1 is the most probable
     if(e.reaction == 1){
+      // create the kinematics
       e.enu = fShape1->GetRandom(gRandom);
       e.pmu = std::max( 0., e.enu*(0.95 + 0.05*gRandom->Gaus(0, 1)) - 0.1 );
       e.pp = 0.1*fShape1->GetRandom(gRandom);
 
+      // how has it been detected?
       buffer = gRandom->Rndm();
-      if     ( buffer < 0.7 ){ e.topology = 1; }
-      else if( buffer < 0.85 ){ e.topology = 2; }
-      else if( buffer < 0.95 ){ e.topology = 3; }
-      else{ e.topology = -1; } // invisible
+      if     ( buffer < 0.8  ){ e.topology =  1; }
+      else if( buffer < 0.85 ){ e.topology =  2; }
+      else if( buffer < 0.95 ){ e.topology =  3; }
+      else                    { e.topology = -1; } // invisible
     }
     else {
       e.enu = fShape2->GetRandom(gRandom);
@@ -58,11 +62,11 @@ void buildInputRootFile(){
       e.pp = 0.1*fShape2->GetRandom(gRandom);
 
       buffer = gRandom->Rndm();
-      if     ( buffer < 0.1 ){ e.topology = 1; }
-      else if( buffer < 0.5 ){ e.topology = 2; }
-      else if( buffer < 0.75 ){ e.topology = 3; }
-      else if( buffer < 0.85 ){ e.topology = 4; }
-      else{ e.topology = -1; }
+      if     ( buffer < 0.15 ){ e.topology =  1; }
+      else if( buffer < 0.2  ){ e.topology =  2; }
+      else if( buffer < 0.85 ){ e.topology =  3; }
+      else if( buffer < 0.95 ){ e.topology =  4; }
+      else                    { e.topology = -1; }
     }
 
     tree->Fill();
